64fd95d193a2a69185ecbd25680c503239d183da
Nicole Raine Cabasa <Nicole Raine Cabasa>
2017-11-14 17:34:22 +0800

    access added

--- project.py
+++ project.py
@@ -26,8 +26,32 @@ def spcall(qry, param, commit=False):
 def index():
     return "Hi!"
 
+@app.route('/access', methods =['POST'])
+def signin():
+    un = request.form['admin']
+    pw =request.form['password']
+
+    res =spcall('checkaccess', (un, pw), True)
+
+    return jsonify({'status': res[0][0]})
 
 
+
+
+
+@app.after_request
+def add_cors(resp):
+    resp.headers['Access-Control-Allow-Origin'] = flask.request.headers.get('Origin', '*')
+    # resp.headers['Access-Control-Allow-Origin'] = flask.request.headers.get ('Origin')
+    resp.headers['Access-Control-Allow-Credentials'] = True
+    resp.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS, GET, PUT, DELETE'
+    resp.headers['Access-Control-Allow-Headers'] = flask.request.headers.get('Access-Control-Request-Headers',
+                                                                             'Authorization')
+    # set low for debugging
+
+    if app.debug:
+        resp.headers["Access-Control-Max-Age"] = '1'
+    return resp
 
 
 if __name__ == '__main__':
